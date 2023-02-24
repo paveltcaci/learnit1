aa0d339b74a7dd9ed435ecae3243e284c55589da
paveltcaci <paveltcaci01@gmail.com>
2023-02-24 17:02:03 +0200

    task3done

--- Setup java Project/task3/src/Main.java
+++ Setup java Project/task3/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner myobj = new Scanner(System.in);
+        int input1 = myobj.nextInt();
+        int input2 = myobj.nextInt();
+
+        int addresult = input1 + input2;
+        int substractionresult = input1 - input2;
+        int multiplyresult = input1 * input2;
+        int divisionresult = input1 / input2;
+        int moduloresult = input1 % input2;
+
+        System.out.println( input1 + " + " + input2+ " = " + addresult);
+        System.out.println( input1 + " - " + input2+ " = " + substractionresult);
+        System.out.println( input1 + " x " + input2+ " = " + multiplyresult);
+        System.out.println( input1 + " / " + input2+ " = " + divisionresult);
+        System.out.println( input1 + " mod " + input2+ " = " + moduloresult);
+
     }
 }
