1fcf6110cba63d47043a936763033d77481c4857
paveltcaci <paveltcaci01@gmail.com>
2023-02-24 16:47:40 +0200

    task2done

--- Setup java Project/Task2/src/Main.java
+++ Setup java Project/Task2/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int result1 = a+b*c;
+
+        int e = 55;
+        int f = 9;
+        int result2 = (e+f)/f;
+
+        int h = 20;
+        int i = -3;
+        int k = 5;
+        int result3 = h+i*k/b;
+
+        int m = 2;
+        int o = 3;
+        int p = 15;
+        int result4 = k+p/o*m-b/o;
+
+        System.out.println(result1+"\n"+result2+"\n"+result3+"\n"+result4);
+
     }
 }
