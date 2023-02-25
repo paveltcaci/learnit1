3afb118ac2b675318aaf50e902c62585ebc67578
paveltcaci <paveltcaci01@gmail.com>
2023-02-25 13:02:48 +0200

    task7done

---
+++ Setup java Project/Task7/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner inputs = new Scanner(System.in);
+
+        String input1 = inputs.next();
+        int decimal=Integer.parseInt(input1,2);
+        System.out.println(decimal);
+    }
+}
